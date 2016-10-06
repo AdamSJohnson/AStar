c85cf6c9f006affdc0e61d1f1d9d54766fa804b6
Adam Johnson <johnsoas198@potsdam.edu>
2016-10-05 21:50:43 -0400

    Started to add the foundations for the tree to use in the AStar search, added an expand method to the ASTNode object to expand its children based on the edges of the Node

--- AStar.java
+++ AStar.java
@@ -3,8 +3,13 @@ import java.util.*;
 
 public class AStar{
     public static void main(String[] args) throws FileNotFoundException{
+        //yes my data file is hard coded
         File f = new File("Data.dat");
+        
+        //setup a scanner on that file
         Scanner sc = new Scanner(f);
+        
+        //create my map
         Map<String, Node> map = new HashMap<>();
         
         //go through each line in the file
@@ -44,6 +49,115 @@ public class AStar{
         //System.out.println(map);
         
         //at this point the map is assembled we can start doing our
+        //heuristical stuff
+        boolean s = true;
+        Scanner ls = new Scanner(System.in);
+        String start = "";
+        while(s){
+            System.out.println("Please pick a city to start from:");
+            for(String name : map.keySet()){
+                System.out.println(name.replaceAll("_", " "));
+            }
+            start = ls.nextLine();
+            System.out.println(start);
+            if(map.containsKey(start.replaceAll(" ", "_"))){
+                s = !s;
+                System.out.println("City: " + start);
+            } else {
+                System.out.println("INVALID CITY press enter");
+                ls.nextLine();
+            } 
+            
+        }
+        
+        
+        //at this point our map is setup and our start point is created WOO
+        
+        
+    }
+    
+}
+
+//this object will do all the work like normal
+class AStarTree{
+    ASTNode root;
+    Map map;
+    public AStarTree(ASTNode r, Map m){
+        this.root = r;
+        this.map = m;
+    }
+    
+}
+
+//the nodes used in the AStarTree
+class ASTNode{
+    //the node should point to its parent node
+    ASTNode parent;
+    
+    //the node should know its children's psuedo root
+    ASTNode child;
+    
+    //the node hsould know its siblings
+    ASTNode leftSib;
+    ASTNode rightSib;
+    
+    //the node should know which city it represents
+    Node city;
+    
+    //the node should know its g(n) aka cost so far
+    Double gcost;
+    
+    //creates an ASTNode with no childen
+    public ASTNode(ASTNode p, ASTNode r, ASTNode l, Node c, double g){
+        this.parent = p;
+        this.rightSib = r;
+        this.leftSib = l;
+        this.city = c;
+        gcost = g;
+        
+    }
+    
+    public void setChild(ASTNode c){
+        this.child = c;
+    }
+    
+    public void expand(Map m){
+        //we cannot expand a node if its child is null
+        if(child != null){
+            throw new IllegalArgumentException();
+        }
+        
+        ASTNode nul = null;
+        ASTNode left = null;
+        ASTNode curr = null;
+        //at this point we can create the children based on the edges of the nodes
+        for(int i = 0; i < city.getEdges().length; i++){
+            //grab the node associated with the edge name
+            Node n = (Node) m.get(city.getEdges()[i].to);
+            
+            //create a new ASTNode based on this node as the parent, the right nodes
+            //null, the left is the left node and the g(n) will be this gcost plus
+            //the cost to traverse the edge
+            curr = new ASTNode(this, nul, left,n,this.gcost + city.getEdges()[i].distance);
+            
+            //if the left node is not null the make its right sibling the current 
+            //node
+            if(left != null){
+                left.rightSib = curr;
+                
+            //if the left node is null then the curr node is the child this node
+            //should point to for the children
+            } else {
+                child = curr;
+           }
+        }
+        //at this point we have taken each edge from the node and expaneded them 
+        //into children of this node. Each child contains its parent, the node 
+        //it represents, and pointers to its siblings 
+    }
+    
+    public boolean expaneded(){
+        return child != null;
     }
     
 }
@@ -60,6 +174,10 @@ class Node{
     
     public String toString(){
         return Arrays.toString(edges);
+    }
+    
+    public Edge[] getEdges(){
+        return edges;
     }
 }
 
